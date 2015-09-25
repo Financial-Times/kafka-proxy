f12f7f26c0d8d1313869790a73a6ef8e466e67be
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-09-25 17:21:23 +0300

    first version of puppet module

---
+++ ft-kafka_proxy/manifests/init.pp
@@ -0,0 +1,31 @@
+class kafka_proxy {
+
+  $confluent = "confluent-1.0"
+  $config_file = "/etc/kafka-proxy.properties"
+
+  class { 'common_pp_up': }
+  class { "${module_name}::monitoring": }
+
+  yumrepo {
+    $confluent:
+      name     => "Confluent repository for 1.x packages",
+      baseurl  => "http://packages.confluent.io/rpm/1.0",
+      gpgcheck => 1,
+      gpgkey   => "http://packages.confluent.io/rpm/1.0/archive.key",
+      enabled  => 1
+  }
+
+  package {
+    "confluent-kafka-rest-2.11.5":
+      ensure          => installed,   
+      require         => Yumrepo[$confluent]
+  }
+
+
+  file {
+    $config_file:
+      content => template("$module_name/kafka-proxy.properties.erb"),
+      mode    => "0664";
+  }
+
+ }
