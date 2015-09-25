55a026647d69c93a10ff6eb6798d9a01ce9e7d8c
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-09-25 17:43:41 +0300

    fixed path

--- scripts/deploy-to-forge.sh
+++ scripts/deploy-to-forge.sh
@@ -3,7 +3,7 @@ set -x
 export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/sbin:/bin
 
 start_time=`date +%s`
-MODULEFULLPATH="$WORKSPACE/puppet/ft-kafka_proxy"
+MODULEFULLPATH="$WORKSPACE/ft-kafka_proxy"
 
 /usr/local/bin/forge-admin.py --publish --source "$MODULEFULLPATH"
 ERROR_CODE=$?
