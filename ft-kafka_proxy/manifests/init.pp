83fd422b40e0f725cfdfe96e2f2f43d290dfaca7
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-09-28 14:36:38 +0300

    revert prev change

--- ft-kafka_proxy/manifests/init.pp
+++ ft-kafka_proxy/manifests/init.pp
@@ -31,7 +31,7 @@ class kafka_proxy {
     'stop-kafka-rest-proxy':
       command		=> "/usr/bin/kafka-rest-stop",
       subscribe		=> [ Package[$kafka_rest], File[$config_file] ],
-      refreshonly	=> false
+      refreshonly	=> true
   }
 
   exec {
