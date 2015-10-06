2dde0236c85e4944b032dcdcf8d4232a4e538240
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-10-06 18:47:03 +0300

    implemented changes after code review

--- ft-kafka_proxy/manifests/init.pp
+++ ft-kafka_proxy/manifests/init.pp
@@ -37,10 +37,11 @@ class kafka_proxy {
 
   exec {
     'restart-kafka-rest-proxy':
-      command		=> "/usr/bin/kafka-rest-stop || true && nohup /usr/bin/kafka-rest-start $config_file &",
+      command		=> "/usr/bin/kafka-rest-stop || nohup /usr/bin/kafka-rest-start $config_file &",
       subscribe		=> [ Package[$kafka_rest], File[$config_file], File[$log_file], Class['jdk'] ],
-      logoutput		=> true,
       refreshonly	=> true
   }
+
+  Exec['enforce_jdk_used'] ~> Service['kafka-proxy']
 
 }
