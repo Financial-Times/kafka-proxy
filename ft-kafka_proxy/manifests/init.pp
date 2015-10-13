3c047433288554b9a1f8937dba94cdb6b12b79eb
Zoltan Bodor <Zoltan.Bodor@ft.com>
2015-10-13 18:38:57 +0300

    replace daemon flag, as it's not working NOTE: it has been fixed on kafka-rest master, but not in the distributed yum package

--- ft-kafka_proxy/manifests/init.pp
+++ ft-kafka_proxy/manifests/init.pp
@@ -37,7 +37,7 @@ class kafka_proxy {
 
   exec {
     'restart-kafka-rest-proxy':
-      command		=> "/usr/bin/kafka-rest-stop ; /usr/bin/kafka-rest-start -daemon $config_file",
+      command		=> "/usr/bin/kafka-rest-stop ; nohup /usr/bin/kafka-rest-start $config_file &",
       subscribe		=> [ Package[$kafka_rest], File[$config_file], Class['jdk'] ],
       refreshonly	=> true
   }
